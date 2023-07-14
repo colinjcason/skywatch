5ec9365d195dcee60fbce306bf02b1ec465855d6
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2023-07-13 21:39:32 -0700

    added loading spinner during load times

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchBox from './components/search-box/SearchBox';
 import Forecast from './components/forecast-card/Forecast';
 import HourlyForecast from './components/hourly-forecast/HourlyForecast';
 import Switch from './components/theme-switcher/Switch'
+import { Oval } from 'react-loader-spinner'
 import './App.css';
 
 function App() {
@@ -16,44 +17,49 @@ function App() {
   const [hourly, setHourly] = useState([])
   const [searchField, setSearchField] = useState('san diego')
   const [theme, setTheme] = useState('light')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    getWeather()
+    setTimeout(() => {
+      getWeather()
+    }, 1000)
   }, [])
 
   useEffect(() => {
     document.body.className = theme;
   }, [theme]);
 
-  const getWeather = () => {    
-    if(searchField === '') {
-      alert('Please enter a city for weather data.')
-    } else {
-      return fetch(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${searchField}&days=3&aqi=no&alerts=yes`)
-      .then(res => {
-        if(!res.ok) {
-          throw new Error(res.status)
-        } else {
-          return res.json()
-        }
-      })
-      .then(data => {
-        setCurrentTemp(data.current.temp_f)
-        setDescription(data.current.condition.text)
-        setIcon(data.current.condition.icon)
-        setHiTemp(data.forecast.forecastday[0].day.maxtemp_f)
-        setLowTemp(data.forecast.forecastday[0].day.mintemp_f)
-        setForecast(data.forecast.forecastday)
-        setHourly(data.forecast.forecastday[0].hour)
-        setLocation(data.location.name)
-        setSearchField('')
-      })
-      .catch(error => {
-        alert('Please enter a valid city name.')
-        setSearchField('')
-        console.error('There was a problem with the Fetch operation:', error)
-      })
-    }
+  const getWeather = () => {
+    setLoading(true)
+      if(searchField === '') {
+        alert('Please enter a city for weather data.')
+      } else {
+        return fetch(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${searchField}&days=3&aqi=no&alerts=yes`)
+        .then(res => {
+          if(!res.ok) {
+            throw new Error(res.status)
+          } else {
+            return res.json()
+          }
+        })
+        .then(data => {
+          setCurrentTemp(data.current.temp_f)
+          setDescription(data.current.condition.text)
+          setIcon(data.current.condition.icon)
+          setHiTemp(data.forecast.forecastday[0].day.maxtemp_f)
+          setLowTemp(data.forecast.forecastday[0].day.mintemp_f)
+          setForecast(data.forecast.forecastday)
+          setHourly(data.forecast.forecastday[0].hour)
+          setLocation(data.location.name)
+          setSearchField('')
+          setLoading(false)
+        })
+        .catch(error => {
+          alert('Please enter a valid city name.')
+          setSearchField('')
+          console.error('There was a problem with the Fetch operation:', error)
+        })
+      }
   }
 
   const handleChange = (e) => {
@@ -74,30 +80,46 @@ function App() {
 
   return (
     <div className={`App ${theme}`}>
-      <Switch handleThemeSelection={handleThemeSelection} theme={theme} />
-
-      <div className='stats'> 
-        <h2 className='location'>{location}</h2>
-        <h1 className='main-temp'>{Math.round(currentTemp)}°</h1>
-        <div className='description-container'>
-          <p id='description'>{description}</p>
-          <img src={icon} alt='weather condition'/>
-        </div>
-        <div className='hi-lo'>
-          <p>H:{Math.round(hiTemp)}°</p>
-          <p>L:{Math.round(lowTemp)}°</p>
-        </div>
-      </div>
-
-      <SearchBox 
-        searchField={searchField}
-        handleChange={handleChange}
-        handleSearch={handleSearch}
-      />
-
-      <HourlyForecast hourly={hourly} />
-
-      <Forecast forecast={forecast} />
+      {loading ? 
+        <Oval
+          height={160}
+          width={160}
+          color={theme === 'light' ? '#f8fafc' : 'rgb(234, 56, 141)'}
+          wrapperStyle={{}}
+          wrapperClass="loader"
+          visible={true}
+          ariaLabel='oval-loading'
+          secondaryColor={theme === 'light' ? "#f0e5e4" : 'rgb(123, 91, 199)'}
+          strokeWidth={2.5}
+          strokeWidthSecondary={2.5}
+        /> : 
+        <>
+          <Switch handleThemeSelection={handleThemeSelection} theme={theme} />
+  
+          <div className='stats'> 
+            <h2 className='location'>{location}</h2>
+            <h1 className='main-temp'>{Math.round(currentTemp)}°</h1>
+            <div className='description-container'>
+              <p id='description'>{description}</p>
+              <img src={icon} alt='weather condition'/>
+            </div>
+            <div className='hi-lo'>
+              <p>H:{Math.round(hiTemp)}°</p>
+              <p>L:{Math.round(lowTemp)}°</p>
+            </div>
+          </div>
+  
+          <SearchBox 
+            searchField={searchField}
+            handleChange={handleChange}
+            handleSearch={handleSearch}
+          />
+    
+          <HourlyForecast hourly={hourly} />
+    
+          <Forecast forecast={forecast} />        
+        </>
+      }
     </div>
   );
 }
